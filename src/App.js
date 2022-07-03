ab52355b1d44d3aab85cc0dd082c4cb08fcbd76d
bushra <bushra.azbm@gmail.com>
2022-07-03 22:57:54 +0300

    solved hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component{
+  
+ state={
+      tasks: []
+    }
+
+    addTask=()=>{
+    
+      this.setState({tasks: this.state.tasks.concat(document.getElementById("userTask").value)});
+
+    }
+
+  render(){
+
+    let todoList = this.state.tasks.map((task) => {
+      return <li>{task}</li>
+  
+    });
+
+    return(
+      <div className='App'>
+    <div className='tasks'>
+    <h1>Tasks:</h1>
+    <input type="text" id="userTask"/> 
+    <button type="button" onClick={this.addTask}>Add Task</button>
+    <ul>
+    {todoList}
+    </ul> 
+      </div> 
+      </div>
+    )
+  }}
+
 
 export default App;
+
